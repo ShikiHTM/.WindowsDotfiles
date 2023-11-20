61bdd6e74db180535d291ecb66761ea128ff467a
ShikiHTM <kizukusan123@gmail.com>
2023-11-20 19:16:41 +0700

    Update Install_Module.ps1

--- Install_Module.ps1
+++ Install_Module.ps1
@@ -10,3 +10,7 @@ irm get.scoop.sh | iex
 
 # PSReadLine
 Install-Module PSReadLine -AllowPrerelease -Force
+
+# Install Vim-Plug
+iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+    ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
